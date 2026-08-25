570cde9cd55b67682c039a9c1e7df0a65fe4127d
Sirmrmagoo <126302009+Sirmrmagoo@users.noreply.github.com>
2026-08-24 21:52:01 -0700

    Add files via upload

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -1,27 +1,31 @@
 { self, inputs, ... }:  {
-  flake.nixosModules.Immich = { config, pkgs, pkgs-unstable, lib, ... }:  {
+  flake.nixosModules.Immich = { config, pkgs, pkgs-unstable, lib, ... }:  
   
-    services.immich = {
-      enable = true;
-      port = 2283;
-      host = "0.0.0.0";
-      package = pkgs-unstable.immich;
-      openFirewall = true;
-      database.enable = true;
-      mediaLocation = "/mnt/NAS/Immich/immichLibrary";
-      accelerationDevices = [ "/dev/dri/by-path/pci-0000:01:00.0-render" ];
-    };
+    let
+      pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
+    in
+    {
+        services.immich = {
+        enable = true;
+        port = 2283;
+        host = "0.0.0.0";
+        package = pkgs-unstable.immich;
+        openFirewall = true;
+        database.enable = true;
+        mediaLocation = "/mnt/NAS/Immich/immichLibrary";
+        accelerationDevices = [ "/dev/dri/by-path/pci-0000:01:00.0-render" ];
+        };
 
-    services.immich.machine-learning.environment = {
-      HF_XET_CACHE = "/var/cache/immich/huggingface-xet";
-    };
+        services.immich.machine-learning.environment = {
+        HF_XET_CACHE = "/var/cache/immich/huggingface-xet";
+        };
 
 
-    users.users."immich" = {
-      home = "/var/lib/immich";
-      createHome = true;
-      extraGroups = [ "wheel" "video" "render" ];
-    };
+        users.users."immich" = {
+        home = "/var/lib/immich";
+        createHome = true;
+        extraGroups = [ "wheel" "video" "render" ];
+        };
     
-  };
+    };
 }
